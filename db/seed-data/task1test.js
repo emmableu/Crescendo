2c7369182e22b5504a27fa78ad7eda363e9c782a
wwang33 <wwang33@ncsu.edu>
2019-06-10 14:54:18 -0400

    06-10-14-54

--- db/seed-data/task1test.js
+++ db/seed-data/task1test.js
@@ -1,41 +1,39 @@
-var id = 'somevarid';
-var isEDX = isEDXurl();
-var graded = true;
-var showFeedback = true;
-var regradeOn = true;
-
-function AGTest(outputLog) {
-    var fb = new FeedbackLog(
-        world,
-        id
-
-     );
-    var blocks =["move %n steps", "stamp"];
-    var i = 0;
-    var len = blocks.length;
-    for (; i < len; ) {
-        block = blocks[i];
-        blockImg = AG_UTIL.HTMLFormattedBlock(block);
-        chunk = fb.newChunk('');
-
-        var tip = chunk.newTip(
-            'Make sure you name your block exactly ' + blockImg + ' and place it in the scripting area.',
-            'Found the ' + blockImg + ' block in the scripting area.'
-        );
-
-        var blockExists = function () {
-            return spriteContainsBlock(block);
-        }
-
-        tip.newAssertTest(
-            blockExists,
-            "Testing if the " + blockImg + " block is in the scripting area.",
-            "The " + blockImg + " block is in the scripting area.",
-            "The " + blockImg + " is not in the scripting area",
-            0
-        );
-        i++;
-    }
-    return fb;
-}
-
+// var id = 'somevarid';
+// var isEDX = isEDXurl();
+// var graded = true;
+// var showFeedback = true;
+// var regradeOn = true;
+//
+// function AGTest(outputLog) {
+//     var fb = new FeedbackLog(
+//         world,
+//         id
+//
+//      );
+//     var blocks =["draw a square with %n size", "stamp"];
+//     var i = 0;
+//     var len = blocks.length;
+//         block = blocks[i];
+//         chunk1 = fb.newChunk('');
+//
+//         var tip = chunk.newTip(
+//             "Draw a s"
+//             'Make sure you name your block exactly ' + blockImg + ' and place it in the scripting area.',
+//             'Found the ' + blockImg + ' block in the scripting area.'
+//         );
+//
+//         var blockExists = function () {
+//             return spriteContainsBlock(block);
+//         }
+//
+//         tip.newAssertTest(
+//             blockExists,
+//             "Testing if the " + blockImg + " block is in the scripting area.",
+//             "The " + blockImg + " block is in the scripting area.",
+//             "The " + blockImg + " is not in the scripting area",
+//             0
+//         );
+//         i++;
+//     return fb;
+// }
+//
