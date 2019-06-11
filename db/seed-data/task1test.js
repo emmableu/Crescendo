199f6fcf9a0d4bd8b142802b9b540cb3c1fb4da7
wwang33 <wwang33@ncsu.edu>
2019-06-11 12:56:42 -0400

    before meeting

--- db/seed-data/task1test.js
+++ db/seed-data/task1test.js
@@ -1,3 +1,66 @@
+var id = 'somevarid';
+var isEDX = isEDXurl();
+var graded = true;
+var showFeedback = true;
+var regradeOn = true;
+
+function AGTest(outputLog) {
+    var fb = new FeedbackLog(
+        world,
+        id
+
+     );
+
+    chunk1 = fb.newChunk('');
+
+    var tip = chunk1.newTip(
+        "Draw a newtip1s",
+        'newtip2',
+        'newtip3'
+    );
+
+    var blockExists = function () {
+        return findBlockInPalette("draw a square with %s size");
+
+    };
+
+    tip.newAssertTest(
+        blockExists,
+        "newasserttest1",
+        "newasserttest2",
+        "newasserttest3",
+        0
+    );
+
+    var blocks =["draw a square with %n size", "stamp"];
+    block = blocks[0];
+    chunk2 = fb.newChunk('');
+
+    var tip = chunk2.newTip(
+        "chunk2 tip 1",
+        'chunk2 tip 2',
+        'chunk2 tip 3'
+    );
+
+    var blockExists = function () {
+        return spriteContainsBlock(block);
+    };
+
+    tip.newAssertTest(
+        blockExists,
+        "chunk2 assert test 1",
+        "chunk2 assert test 2",
+        "chunk2 assert test 3",
+        0
+    );
+    return fb;
+}
+
+
+
+
+
+
 // var id = 'somevarid';
 // var isEDX = isEDXurl();
 // var graded = true;
